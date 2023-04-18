03e2692631e1602f9b3e4a4bb9b1a2b705c0a53b
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+github@bsc.es>
2023-04-18 12:13:02 +0200

    Move Julia code to new file

---
+++ register.jl
@@ -0,0 +1,75 @@
+using Pkg
+using Pkg: GitTools
+using RegistryTools
+using HTTP: URI
+using LibGit2
+
+pkg_url = String(readchomp(`git remote get-url origin`))
+println("::info::Repository = $pkg_url")
+
+project = RegistryTools.Project("Project.toml")
+isnothing(project) && error("Project file not found")
+println("::info::Project name = $(project.name)")
+println("::info::Project UUID = $(project.uuid)")
+println("::info::Project version = $(project.version)")
+
+tree_hash = String(readchomp(`git rev-parse HEAD^\{tree\}`))
+println("::info::Tree hash = $tree_hash")
+
+const private_reg_url = GitTools.normalize_url("${{ inputs.registry }}")
+const general_reg_url = "https://github.com/JuliaRegistries/General"
+println("::info::Registry = $private_reg_url")
+
+registry_repo = RegistryTools.get_registry(private_reg_url; force_reset=false)
+println("::info::Registry path = $registry_repo")
+
+const branch = get(ENV, "INPUTS_BRANCH", RegistryTools.registration_branch(project; url=pkg_url))
+println("::info::Registry branch = $branch")
+
+cd(mktempdir()) do
+    # adapted from fregante/setup-git-user@v1, https://stackoverflow.com/a/71984173
+    regbranch = RegistryTools.register(pkg_url, project, tree_hash;
+        registry=private_reg_url,
+        registry_deps=[general_reg_url],
+        push=${{inputs.push}},
+        branch=branch,
+        gitconfig=Dict(
+            "user.name" => "${{ inputs.name }}",
+            "user.email" => "${{ inputs.email }}",
+            "url.$(string(URI(URI(private_reg_url); userinfo="${{ inputs.name }}:$(ENV["GITHUB_TOKEN"])"))).insteadOf" => private_reg_url
+        ))
+    println("::info::RegBranch = $regbranch")
+
+    if haskey(regbranch.metadata, "error")
+        if regbranch.metadata["kind"] == "New version" && regbranch.metadata["error"] == "Version $(project.version) already exists"
+            println("::warning file=Project.toml,line=3,col=11,endColumn=$(11 + 1 + length(string(project.version))),title=Package not registered::Version $(project.version) already exists")
+            return 0
+        else
+            println("::error file=Project.toml,title=$(regbranch.metadata["kind"])::$(regbranch.metadata["error"])")
+            error(regbranch.metadata["error"])
+        end
+    end
+end
+
+open(ENV["GITHUB_OUTPUT"], "w") do io
+    println(io, "name=$(project.name)")
+    println(io, "uuid=$(project.uuid)")
+    println(io, "version=$(project.version)")
+    println(io, "hash=$tree_hash")
+    println(io, "branch=$branch")
+    println(io, "path=$(LibGit2.path(registry_repo))")
+end
+
+open(ENV["GITHUB_STEP_SUMMARY"], "w") do io
+    println(
+        io,
+        """
+    # Package registered :package:
+
+    - Registry: $private_reg_url
+    - Project: $(project.name)
+    - UUID: $(project.uuid)
+    - Version: $(project.version)
+"""
+    )
+end
