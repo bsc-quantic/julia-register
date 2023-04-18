252682094f16a10d782b74d88512eda66912a734
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+github@bsc.es>
2023-04-18 19:15:04 +0200

    Fix module

--- register.jl
+++ register.jl
@@ -73,7 +73,7 @@ cd(mktempdir()) do
     )
     @info "Pull Request contents" title = params["title"] body = params["body"]
 
-    auth = Registrator.get_user_auth()
+    auth = Registrator.CommentBot.get_user_auth()
     pr = GitHub.create_pull_request(registry, auth=auth, params=params)
     GitHub.add_labels(repo, pr, lowercase.(regbranch.metadata["labels"]), auth=auth)
 end
